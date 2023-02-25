3904315a048c18c1c9e87db30cec449a6f2c37fa
Sudeep4686 <sudeephipparge@gmail.com>
2023-02-25 10:29:06 +0530

    firstform

---
+++ dom1.js
@@ -0,0 +1,17 @@
+// console.dir(document)
+//console.log(document.domain);
+//console.log(document.URL)
+//console.log(document.title);
+//console.log(document.forms);
+var headerTitle = document.getElementById('header-title');
+var header=document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent="Hello123";
+//headerTitle.innerText='GoodBye';
+//console.log(headerTitle.innerText);
+//headerTitle.innerHTML='<h3>Hello</h3>';
+header.style.borderBottom = 'solid 3px #000';
+headerTitle.style.textDecoration
+const itemsheading=document.querySelector('.title:first-of-type');
+itemsheading.style.color='green';
+itemsheading.style.fontWeight = 'bold';
