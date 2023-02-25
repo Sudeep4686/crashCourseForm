48098b72b95eecb53b8885ae9ca94a834b62a899
Sudeep4686 <sudeephipparge@gmail.com>
2023-02-25 11:43:41 +0530

    fontchanged

--- dom1.js
+++ dom1.js
@@ -10,8 +10,17 @@ var header=document.getElementById('main-header');
 //headerTitle.innerText='GoodBye';
 //console.log(headerTitle.innerText);
 //headerTitle.innerHTML='<h3>Hello</h3>';
-header.style.borderBottom = 'solid 3px #000';
-headerTitle.style.textDecoration
-const itemsheading=document.querySelector('.title:first-of-type');
-itemsheading.style.color='green';
-itemsheading.style.fontWeight = 'bold';
+//header.style.borderBottom = 'solid 3px #000';
+//headerTitle.style.textDecoration
+//const itemsheading=document.querySelector('.title:first-of-type');
+//itemsheading.style.color='green';
+//itemsheading.style.fontWeight = 'bold';
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='hello';
+items[2].style.backgroundColor='yellow';
+items[0].style.fontWeight='bold'
+items[1].style.fontWeight='bold'
+items[2].style.fontWeight='bold'
+items[3].style.fontWeight='bold'
