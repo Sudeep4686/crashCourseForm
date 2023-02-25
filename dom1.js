b0eeb6cd5f2028eb44cdef003bba409f32464972
Sudeep4686 <sudeephipparge@gmail.com>
2023-02-25 12:03:29 +0530

    item5Added

--- dom1.js
+++ dom1.js
@@ -15,12 +15,27 @@ var header=document.getElementById('main-header');
 //const itemsheading=document.querySelector('.title:first-of-type');
 //itemsheading.style.color='green';
 //itemsheading.style.fontWeight = 'bold';
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='hello';
-items[2].style.backgroundColor='yellow';
-items[0].style.fontWeight='bold'
-items[1].style.fontWeight='bold'
-items[2].style.fontWeight='bold'
-items[3].style.fontWeight='bold'
+//var items=document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent='hello';
+//items[2].style.backgroundColor='yellow';
+//items[0].style.fontWeight='bold'
+//items[1].style.fontWeight='bold'
+//items[2].style.fontWeight='bold'
+//items[3].style.fontWeight='bold'
+//for (var i=0; i<items.length; i++){
+//    items[i].style.fontWeight='bold'
+//}
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='hello';
+li[2].style.backgroundColor='yellow';
+//items[0].style.fontWeight='bold'
+//items[1].style.fontWeight='bold'
+//items[2].style.fontWeight='bold'
+//items[3].style.fontWeight='bold'
+for (var i=0; i<li.length; i++){
+    li[i].style.fontWeight='bold'
+}
