78a92055387d8cf4bd34c05c2ee5f013c4fe2584
Sudeep4686 <sudeephipparge@gmail.com>
2023-02-25 12:23:25 +0530

    querySelector

--- dom1.js
+++ dom1.js
@@ -27,15 +27,36 @@ var header=document.getElementById('main-header');
 //for (var i=0; i<items.length; i++){
 //    items[i].style.fontWeight='bold'
 //}
-var li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent='hello';
-li[2].style.backgroundColor='yellow';
-//items[0].style.fontWeight='bold'
-//items[1].style.fontWeight='bold'
-//items[2].style.fontWeight='bold'
-//items[3].style.fontWeight='bold'
-for (var i=0; i<li.length; i++){
-    li[i].style.fontWeight='bold'
-}
+//var li=document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[1]);
+//li[1].textContent='hello';
+//li[2].style.backgroundColor='yellow';
+//for (var i=0; i<li.length; i++){
+//    li[i].style.fontWeight='bold'
+//}
+
+// Query selector //
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #222';
+
+var input = document.querySelector('input');
+input.value = 'Hello World';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value="send";
+
+var item = document.querySelector('.list-group-item');
+item.style.color='red';
+
+var lastItem=document.querySelector('.list-group-item:last-child');
+lastItem.style.color='blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display='None';
+
+
+
